5256b379a5ef6ffa8c22c14c00b1022c4bddfd1f
niallfitzg <niallfitzg@gmail.com>
2023-11-10 12:42:55 +0000

    Project2_Functions Assignment

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,5 +1,11 @@
 function updateContent() {
-    var part = "This is my ";
-    part += "Function Assignment";
+    var part = "This is my Project 2 "; // creating the variable
+    part += "Function Assignment"; // using the += operator to add to the variable
     document.getElementById("assignment").innerHTML = part; // concatentate vars to populate element
+}
+
+function thankYou() {
+    var thanks = "Thank you for ";// creating the variable
+    thanks += "Viewing my Function Assignment"; // using the += operator to add to the variable
+    document.getElementById("thanks_text").innerHTML = thanks; // concatentate vars to populate element
 }
