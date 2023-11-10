3c89fcffb3b35da5f526eaf9b5b492cd93008495
niallfitzg <niallfitzg@gmail.com>
2023-11-10 11:25:43 +0000

    Basic JavaScript Projects Assignment

--- Basic Javascript Project/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Project/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,23 @@
-function My_First_Function() { //Defining and naming a function
 
-    var str = "This is the button text!"; //button text variable
+//Utilize the window.alert() and document.write() methods
+window.alert("Basic Javascript Project Assignment");
+document.write("By Niall Fitzgerald" + "<br><br>");  // Added <br> for line breaks
+
+//Concatenate the Sent1 and Sent2 variables together
+
+var Sent1 = "This is the beginning of the string"
+var Sent2 = " and this is the end of the string" 
+document.write(Sent1 + Sent2 + "<br><br>");  // Added <br> for line breaks
+
+
+//Write an expression
+document.write("The product of 3 times 3 is... ");
+document.write(3 * 3 + "<br></br>");  // Added <br> for line breaks
+
+//A function for a button to say thanks
+function Thanks_Function() { //Defining and naming the function
+
+    var str = "Thanks for viewing my submission!"; //button text variable
 
     document.getElementById("Button_Text").innerHTML = str; //Insert text frrom vaiable into button
 
