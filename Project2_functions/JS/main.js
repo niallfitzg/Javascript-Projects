74852392e0ffaab61a136c2acb15655ed7eac3cc
niallfitzg <niallfitzg@gmail.com>
2023-11-10 12:24:55 +0000

    Function Assignment

--- Project2_functions/JS/main.js
+++ Project2_functions/JS/main.js
@@ -1,1 +1,5 @@
-
+function updateContent() {
+    var part1 = "This is my ";
+    var part2 = "Function Assignment";
+    document.getElementById("assignment").innerHTML = part1 + part2; // concatentate vars to populate element
+}
