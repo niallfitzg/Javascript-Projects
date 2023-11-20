3a24ca60c131b34fbfa550268c78be8685b8d166
niallfitzg <niallfitzg@gmail.com>
2023-11-20 10:48:13 +0000

    Maths Operators
    Subtraction Assignment

--- Basic Javascript Project/Project3_math_operators/JS/main.js
+++ Basic Javascript Project/Project3_math_operators/JS/main.js
@@ -3,3 +3,8 @@ function addition_Function() {
     document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
 }
 
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+}
+
