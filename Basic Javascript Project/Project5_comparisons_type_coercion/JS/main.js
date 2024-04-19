a6ee6de1d15ba9126d248dada083d49047a6fbbb
niallfitzg <niallfitzg@gmail.com>
2024-04-19 19:59:24 +0100

    Infinity Assignment

--- Basic Javascript Project/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Project/Project5_comparisons_type_coercion/JS/main.js
@@ -1,3 +1,12 @@
+//Force Coercion
 document.write("This is Type Coercion using the String 20 and the Number 3: ");
 document.write("20" + 3);
 
+//Return Infinity
+function displayInfinity() {
+    document.getElementById("infinityDisplay").innerHTML = "Infinity: " + (3E310);
+}
+//Return Negative Infinity
+function displayNegInfinity() {
+    document.getElementById("negInfinityDisplay").innerHTML = "Negative Infinity: " + (-3E310);
+}
