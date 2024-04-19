8ebc6c335de342da060e65289eb2866d99a124f1
niallfitzg <niallfitzg@gmail.com>
2024-04-19 18:02:31 +0100

    Dictionary Assignment
    Used a vehicle related dictionary

---
+++ Basic Javascript Project/Project4_dictionaries/JS/main.js
@@ -0,0 +1,18 @@
+//function to add a dictionary
+function my_Dictionary() {
+    var Animal = {
+        Species: "Dog",
+        Color: "Black",
+        Breed: "Labrador",
+        Age: 5,
+        Sound: "Bark!"
+    };
+    
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+}
+
+
+
+
+
+
---
+++ Basic Javascript Project/Project4_javascript data_types/JS/main.js
@@ -0,0 +1,18 @@
+//function to add a dictionary
+function my_Dictionary() {
+    var Car = {
+        Brand: "Ford",
+        Model: "Fiesta",
+        Color: "Black",
+        Fuel: "Petrol",
+        Age: 10,
+    };
+    
+    document.getElementById("Dictionary").innerHTML = "The brand of the vehicle is: " + Car.Brand;
+}
+
+
+
+
+
+
