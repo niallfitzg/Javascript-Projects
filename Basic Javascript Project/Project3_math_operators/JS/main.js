a4512ff7c2b80afee44f7b37e69337c8a53be827
niallfitzg <niallfitzg@gmail.com>
2024-04-19 16:17:50 +0100

    Multiple Operations
    With updated IDs throughout

--- Basic Javascript Project/Project3_math_operators/JS/main.js
+++ Basic Javascript Project/Project3_math_operators/JS/main.js
@@ -1,20 +1,24 @@
 function addition_Function() {
-	var addition = 2 + 2;
-    document.getElementById("Math").innerHTML = "2 + 2 = " + addition;
+    var addition = 2 + 2;
+    document.getElementById("addition").innerHTML = "2 + 2 = " + addition;
 }
 
 function subtraction_Function() {
     var subtraction = 5 - 2;
-    document.getElementById("Math").innerHTML = "5 - 2 = " + subtraction;
+    document.getElementById("subtraction").innerHTML = "5 - 2 = " + subtraction;
 }
 
 function multiplication_Function() {
     var multiply = 6 * 8;
-    document.getElementById("Mult1").innerHTML = "6 * 8 = " + multiply;
+    document.getElementById("multiplication").innerHTML = "6 * 8 = " + multiply;
 }
 
 function division_Function() {
     var divide = 48 / 6;
-    document.getElementById("Div1").innerHTML = "48 / 6 = " + divide;
+    document.getElementById("division").innerHTML = "48 / 6 = " + divide;
 }
 
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 - 5;
+    document.getElementById("moreMath").innerHTML = "1 plus 2, multiplied by 10, divided in half, and then subtracted by 5 equals " + simple_Math;
+}
