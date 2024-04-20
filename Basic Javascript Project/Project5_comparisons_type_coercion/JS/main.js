5668771f6c34cd45620d8c52e9f9166a2e3bc1c8
niallfitzg <niallfitzg@gmail.com>
2024-04-20 08:37:00 +0100

    Not Operator Assignment
    Also tidied up method of displaying results without requiring refresh of page

--- Basic Javascript Project/Project5_comparisons_type_coercion/JS/main.js
+++ Basic Javascript Project/Project5_comparisons_type_coercion/JS/main.js
@@ -13,12 +13,12 @@ function displayNegInfinity() {
 
 //Return Boolean True
 function displayBooleanTrue() {
-    document.getElementById("booleanTrue").innerHTML = document.write(10 > 2);
+    document.getElementById("booleanTrue").innerHTML = "Boolean on (10 > 2) is: " + (10 > 2);
 }
 
 //Return Boolean False
 function displayBooleanFalse() {
-    document.getElementById("booleanFalse").innerHTML = document.write(10 < 2);
+    document.getElementById("booleanFalse").innerHTML = "Boolean on (10 < 2)  is: " + (10 < 2);
 }
 
 //Output Console  Log
@@ -29,61 +29,72 @@ function displayConsoleLog() {
 
 //Output Double Equals True Value
 function doubleEqualsTrue() {
-    document.getElementById("doubleEqualsTrue").innerHTML = document.write(10 == 10);
+    document.getElementById("doubleEqualsTrue").innerHTML = "Double Equals (10 == 10) is: " + (10 == 10);
 }
 
 //Output Double Equals False Value
 function doubleEqualsFalse() {
-    document.getElementById("doubleEqualsFalse").innerHTML = document.write(3 == 11);
+    document.getElementById("doubleEqualsFalse").innerHTML = "Double Equals (3 == 11) is: " + (3 == 11);
 }
 
 //Output Triple Equals True Value - matching data type and value
 function tripleEqualsTrue() {
     X = 10;
     Y = 10;
-    document.getElementById("tripleEqualsTrue").innerHTML = document.write(X === Y);
+    document.getElementById("tripleEqualsTrue").innerHTML = "Triple Equals 10 integer = 10 integer is: " + (X === Y);
 }
 
 //Output Triple Equals False Value - different data type and value
 function tripleEqualsFalse1() {
     X = "Niall";
     Y = 10;
-    document.getElementById("tripleEqualsFalse1").innerHTML = document.write(X === Y);
+    document.getElementById("tripleEqualsFalse1").innerHTML = "Triple Equals 'Niall' string = 10 integer is: " + (X === Y);
 }
 
 //Output Triple Equals False Value - different data type but the same value for both.
 function tripleEqualsFalse2() {
     X = "10";
     Y = 10;
-    document.getElementById("tripleEqualsFalse2").innerHTML = document.write(X === Y);
+    document.getElementById("tripleEqualsFalse2").innerHTML = "Triple Equals '10' string = 10 integer is: " + (X === Y);
 }
 
 //Output Triple Equals False Values - same data type but a different value for both.
 function tripleEqualsFalse3() {
     X = "Niall";
     Y = "John";
-    document.getElementById("tripleEqualsFalse3").innerHTML = document.write(X === Y);
+    document.getElementById("tripleEqualsFalse3").innerHTML = "Triple Equals 'Niall' string = 'John' string is: " + (X === Y);
 }
 
 //Output AND Operator True Value
 function andTrue() {
-    document.getElementById("andTrue").innerHTML = document.write(5 > 2 && 10 > 4);
+    document.getElementById("andTrue").innerHTML = "AND Operator Value on (5 > 2 && 10 > 4) is: " + (5 > 2 && 10 > 4);
 }
 
 //Output AND Operator False Value
 function andFalse() {
-    document.getElementById("andFalse").innerHTML = document.write(5 > 10 && 10 > 4);
+    document.getElementById("andFalse").innerHTML = "AND Operator Value on (5 > 10 && 10 > 4) is: " + (5 > 10 && 10 > 4);
 }
 
 //Output OR Operator True Value
 function orTrue() {
-    document.getElementById("orTrue").innerHTML = document.write(5 > 2 || 10 > 4);
+    document.getElementById("orTrue").innerHTML = "OR Operator Value on (5 > 2 || 10 > 4) is: " + (5 > 2 || 10 > 4);
 }
 
 //Output OR Operator False Value
 function orFalse() {
-    document.getElementById("orFalse").innerHTML = document.write(5 > 2 || 10 > 4);
+    document.getElementById("orFalse").innerHTML = "OR Operator Value on (5 < 2 || 10 > 4) is: " + (5 < 2 || 10 > 4);
 }
+
+//Output NOT Operator True Value
+function notFunction() {
+    document.getElementById("not").innerHTML = "NOT Operator Value on !(20 > 10) is: " + !(20 > 10);
+}
+
+//Output NOT Operator Double Negative
+function notFunction2() {
+    document.getElementById("not2").innerHTML = "NOT Operator Value on !(5 > 10) is: " + !(5 > 10);
+}
+
 
 
 
