5731f0adeb046f64cfcbf9d10f1965a84aa04e59
niallfitzg <niallfitzg@gmail.com>
2024-04-20 12:53:44 +0100

    Nested Functions Assignment

--- Basic Javascript Project/Project6_Ternary_Operators_Constructors/JS/main.js
+++ Basic Javascript Project/Project6_Ternary_Operators_Constructors/JS/main.js
@@ -4,3 +4,13 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short" : "You are tall enough"
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function count_Function() {
+    document.getElementById("Nested_Function").innerHTML = "Number after increment: " + increment_Number();
+
+    function increment_Number() {
+        var myNum = 9;
+        myNum++; 
+        return myNum; 
+    }
+}
