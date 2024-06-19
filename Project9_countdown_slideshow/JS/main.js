bc75d250c0eea604fbdcb79b700a99ac1ea462f5
niallfitzg <niallfitzg@gmail.com>
2024-06-19 15:22:06 +0100

    Countdown Assignment

---
+++ Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,15 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+    
+    function tick() {
+        seconds = seconds - 1;
+        Timer.innerHTML = seconds;
+        var time = setTimeout(tick, 1000);
+        if (seconds == -1) {
+            alert("Time's Up!")
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
