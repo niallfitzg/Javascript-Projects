d75b26b218bfbe1ba3c2f5ebfab67e97a7f5e541
niallfitzg <niallfitzg@gmail.com>
2024-06-19 14:08:01 +0100

    slice() Method Assignment

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,3 +1,4 @@
+//Concatanate Method
 function full_Sentence() {
     var part_1 = "I have ";
     var part_2 = "made this ";
@@ -5,4 +6,11 @@ function full_Sentence() {
     var part_4 = "sentence.";
     var whole_sentence = part_1.concat(part_2, part_3, part_4);
     document.getElementById("Concatenate").innerHTML = whole_sentence;
+}
+
+//SLice Method
+function slice_Method() {
+    var Sentence = "All work and no play makes Johnny a dull boy";
+    var Section = Sentence.slice(27, 33);
+    document.getElementById("Slice").innerHTML = Section;
 }
